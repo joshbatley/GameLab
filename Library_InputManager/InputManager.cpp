fae30e7b72e36531cee011fa12f15e80095fc1be
Josh <jbatley12@gmail.com>
2024-03-07 21:24:04 +0000

    added basic input manager

--- Library_InputManager/InputManager.cpp
+++ Library_InputManager/InputManager.cpp
@@ -1,7 +1,43 @@
 #include "InputManager.h"
 
-#include <iostream>
+void InputManager::Subscribe(EventType type, std::function<void()> callback) {
+    eventListeners[type].push_back(callback);
+}
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+void InputManager::ProcessEvents() {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                dispatchEvent(KEY_PRESSED);
+            case SDL_MOUSEMOTION:
+                mousePosition.x = event.motion.x;
+                mousePosition.y = event.motion.y;
+                dispatchEvent(MOUSE_MOVED);
+                break;
+        }
+    }
+}
+
+void InputManager::dispatchEvent(EventType eventType) {
+    for (auto& listener : eventListeners[eventType]) {
+        listener();
+    }
+}
+
+bool InputManager::IsActionPressed(InputAction action) {
+    auto keycodes = actionMap[action];
+    const Uint8* keyStates = SDL_GetKeyboardState(nullptr);
+
+    for (const auto& keycode : keycodes) {
+        if (keyStates[keycode]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+MousePosition InputManager::GetMousePosition() {
+    return mousePosition;
 }
--- Library_InputManager/InputManager.h
+++ Library_InputManager/InputManager.h
@@ -2,7 +2,42 @@
 #define LIBRARY_INPUTMANAGER_H
 
 #include "SDL.h"
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <unordered_map>
 
-void hello();
+enum EventType {
+    KEY_PRESSED,
+    MOUSE_MOVED,
+};
+
+enum InputAction {
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+struct MousePosition {
+    int x;
+    int y;
+};
+
+
+class InputManager {
+public:
+    void Subscribe(EventType type, std::function<void()> callback);
+    void ProcessEvents();
+    bool IsActionPressed(InputAction key);
+    MousePosition GetMousePosition();
+
+private:
+    std::unordered_map<EventType, std::vector<std::function<void()>>> eventListeners;
+    std::unordered_map<InputAction, std::vector<SDL_KeyCode>> actionMap;
+    MousePosition mousePosition;
+
+    void dispatchEvent(EventType eventType);
+};
 
 #endif// LIBRARY_INPUTMANAGER_H
