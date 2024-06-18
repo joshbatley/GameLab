ac5bed55be18e512f74056d69fc7d80e21c14a92
Josh <jbatley12@gmail.com>
2024-06-18 14:09:57 +0100

    updated Comonents

--- Library_Components/Components.h
+++ Library_Components/Components.h
@@ -1,5 +1,4 @@
 #pragma once
 
-class Components {
-
-};
+#include "Sprite.h"
+#include "Transform.h"
---
+++ Library_Components/Transform.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct Transform {
+    Vec::ivec3 translate {};
+    Vec::ivec3 scale {};
+};
