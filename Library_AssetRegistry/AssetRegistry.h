e2ff8c6beace42fc7ab4e8f7497db79993271ec6
Josh <jbatley12@gmail.com>
2024-06-18 13:58:34 +0100

    updated AssetRegistry

--- Library_AssetRegistry/AssetRegistry.h
+++ Library_AssetRegistry/AssetRegistry.h
@@ -1,5 +1,19 @@
 #pragma once
 
-class AssetRegistry {
+#include "../Alias.h"
+#include <SDL_image.h>
 
-};
+namespace Asset {
+    class Registry {
+    public:
+        explicit Registry(SDL_Renderer *renderer);
+        ~Registry();
+        Engine::Font LoadFont(const std::string &key, const char *path, int size);
+        Engine::Texture LoadTexture(const std::string &key, const char *path);
+
+    private:
+        std::unordered_map<std::string, Engine::Font> _fontMap;
+        std::unordered_map<std::string, Engine::Texture> _textureMap;
+        SDL_Renderer *_renderer;
+    };
+}
